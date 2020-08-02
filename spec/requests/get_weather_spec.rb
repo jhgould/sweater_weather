944760ac56ede81653f8a9ea853aaffdc8be8519
Jack Gould <johnhilsmangould@gmail.com>
2020-08-02 11:26:01 -0600

    add tests

--- spec/requests/get_weather_spec.rb
+++ spec/requests/get_weather_spec.rb
@@ -3,7 +3,6 @@ RSpec.describe 'Test the api request' do
   it "it can retrieve weather for a city" do 
     
     get "/api/v1/forecast?location=denver,co"
-
     expect(response).to be_successful 
 
   end 
