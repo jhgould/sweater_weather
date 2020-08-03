26296aae67386d4989b3a797b677e04e40b56adb
Jack Gould <johnhilsmangould@gmail.com>
2020-08-03 15:08:51 -0600

    add registration functinality

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,8 @@ Rails.application.routes.draw do
 namespace :api do 
   namespace :v1 do 
     resource :forecast, only: [:show]
-    resource :backgrounds, only:[:show] 
+    resource :backgrounds, only: [:show] 
+    resource :users, only: [:create]
   end 
 end 
  
