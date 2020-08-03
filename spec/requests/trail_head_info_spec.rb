4f0bfe054edf4a34070aeeb6e08f4f9a2746c77f
Jack Gould <johnhilsmangould@gmail.com>
2020-08-03 11:09:15 -0600

    add simple test due to lack of time

--- spec/requests/trail_head_info_spec.rb
+++ spec/requests/trail_head_info_spec.rb
@@ -1,11 +1,20 @@
 require 'rails_helper'
 RSpec.describe 'Test trail head location info' do 
+    # it "it can retrieve weather, distance and trail info", :vcr  do 
+      
+    #   get "/api/v1/trail?from=denver,co&to=boulder,co"
+    #   expect(response).to be_successful 
+    #   binding.pry
+    #   # json = JSON.parse(response.body, symbolize_names: true)
+    # end 
+
     it "it can retrieve weather, distance and trail info", :vcr  do 
       
-      get "/api/v1/trail?from=denver,co&to=boulder,co"
+      get "/api/v1/trails?location=denver,co"
       expect(response).to be_successful 
       binding.pry
       # json = JSON.parse(response.body, symbolize_names: true)
     end 
+
 
 end
