7922874f84aced772b79973734e340de2c5729c8
Jack Gould <johnhilsmangould@gmail.com>
2020-08-03 14:14:00 -0600

    add user test and model/update db with email

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,6 @@
 class User < ApplicationRecord
+  validates_presence_of :email, :password, :password_confirmation
+  validates :email, uniqueness: true
+  validates_confirmation_of :password
 
 end 
--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_02_011852) do
+ActiveRecord::Schema.define(version: 2020_08_03_200948) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "users", force: :cascade do |t|
-    t.string "name"
     t.string "password"
     t.string "password_confirmation"
+    t.string "email"
   end
 
 end
