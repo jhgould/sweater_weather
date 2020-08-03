f5dd9996315ca8d187fcd1f2b165fcd0b5a383e5
Jack Gould <johnhilsmangould@gmail.com>
2020-08-03 09:28:14 -0600

    add begining of test

---
+++ spec/requests/trail_head_info_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+RSpec.describe 'Test trail head location info' do 
+    it "it can retrieve weather, distance and trail info", :vcr do 
+      
+      get "/api/v1/trail?from=denver,co&to=boulder,co"
+      expect(response).to be_successful 
+      json = JSON.parse(response.body, symbolize_names: true)
+    end 
+
+end
