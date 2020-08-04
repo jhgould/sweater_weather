d4302a685f511e9c25c0ab108bd70dc237922977
Jack Gould <johnhilsmangould@gmail.com>
2020-08-04 13:38:23 -0600

    add spec for weather daily_weather hourly_weather

--- spec/models/daily_weather_spec.rb
+++ spec/models/daily_weather_spec.rb
@@ -1,14 +1,15 @@
 require 'rails_helper'
-RSpec.describe Background, type: :model do 
+RSpec.describe DailyWeather, type: :model do 
   before :each do 
-    response = File.read("spec/fixtures/postman_fixtures/unsplash_api.json")
+    response = File.read("spec/fixtures/postman_fixtures/weather_api.json")
     @data = JSON.parse(response, symbolize_names: true)
   end 
   it "test background model" do 
-    background = Background.new(@data)
-    expect(background).to be_a Background 
-    expect(background.id).to eq(nil)
-    expect(background.background).to eq("https://images.unsplash.com/photo-1568818196688-d6d23eabeb87?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjE1Mzk3OX0")
+    weather = DailyWeather.new(@data[:daily][0])
+    expect(weather).to be_a DailyWeather
+    expect(weather.desc).to eq("Rain")
+    expect(weather.max).to eq(85.03)
+    expect(weather.min).to eq(63.5)
   end 
 
 end
