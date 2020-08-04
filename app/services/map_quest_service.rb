5d3400b06d5f0cfef88095e423f293afc1ff2c07
Jack Gould <johnhilsmangould@gmail.com>
2020-08-04 14:34:42 -0600

    refactored services

--- app/services/map_quest_service.rb
+++ app/services/map_quest_service.rb
@@ -1,15 +1,17 @@
 class MapQuestService 
 
   def location_data(location)
-    conn = Faraday.new(url: "https://www.mapquestapi.com")
     response = conn.get("/geocoding/v1/address?key=#{ENV["MAP_API_KEY"]}&outFormat=json&location=#{location}&thumbMaps=false")
     location_data = Location.new(json = JSON.parse(response.body, symbolize_names: true))
   end 
 
 
   def travel_time(origin, destination)
-    conn = Faraday.new(url: "https://www.mapquestapi.com")
     response = conn.get("/directions/v2/route?key=#{ENV["MAP_API_KEY"]}&from=#{origin}%2C+CO&to=#{destination}&outFormat=json")
+  end 
+
+  def conn 
+    Faraday.new(url: "https://www.mapquestapi.com")
   end 
 
 end 
--- app/services/weather_service.rb
+++ app/services/weather_service.rb
@@ -1,14 +1,16 @@
 class WeatherService
 
   def weather_info(lat, lng)
-    conn = Faraday.new(url: "https://api.openweathermap.org" )
     response = conn.get("/data/2.5/onecall?lat=#{lat}&lon=#{lng}&appid=#{ENV["WEATHER_API_KEY"]}&units=imperial")
     json = JSON.parse(response.body, symbolize_names: true)
   end 
 
   def current_weather_info(destination)
-    conn = Faraday.new(url: "https://api.openweathermap.org" )
     response = conn.get("/data/2.5/weather?q=#{destination}&appid=#{ENV["WEATHER_API_KEY"]}&units=imperial")
     JSON.parse(response.body, symbolize_names: true)
+  end 
+
+  def conn
+    Faraday.new(url: "https://api.openweathermap.org" )
   end 
 end 
